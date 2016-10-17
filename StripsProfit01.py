d760fc676d47c3e1cb3553218ebc091d0701e387
IASTATE\ebrandes <ebrandes@iastate.edu>
2016-10-17 11:21:33 -0500

    changed the input profit table: included soybean and corn profit surfaces in 2012 and 2014

--- StripsProfit01.py
+++ StripsProfit01.py
@@ -18,23 +18,23 @@ arcpy.env.workspace = "C:\\Users\\ebrandes\\Documents\\DNDC\\switchgrass_integra
 in_feature = "C:\\Users\\ebrandes\\Documents\\shapefiles\\wbdhu12_a_07100004.shp"
 out_path = "C:\\Users\\ebrandes\\Documents\\DNDC\\switchgrass_integration.gdb"
 out_name = "HUC_07100004"
-arcpy.FeatureClassToFeatureClass_conversion(in_feature, out_path, out_name)
+#arcpy.FeatureClassToFeatureClass_conversion(in_feature, out_path, out_name)
 
 # reproject "HUC_07100004"
 in_dataset = "HUC_07100004"
 out_dataset = str(in_dataset) + "_Projected"
 out_coor_system = arcpy.SpatialReference('NAD 1983 UTM Zone 15N')
-arcpy.Project_management(in_dataset, out_dataset, out_coor_system)
+#arcpy.Project_management(in_dataset, out_dataset, out_coor_system)
 
 # check the spatial reference of the new feature class
 desc = arcpy.Describe(out_dataset)
-spatialRef = desc.SpatialReference
-print("Just checking ... Reference System is " + str(spatialRef.Name) + ".")
+#spatialRef = desc.SpatialReference
+#print("Just checking ... Reference System is " + str(spatialRef.Name) + ".")
 
 # profit data (table)
-in_rows ="60_profit_2012_2014_IA151.txt"
+in_rows ="C:\\Users\\ebrandes\\Documents\\STRIPS_profit\\tables\\cgsb_profit_surface_2012_2014.txt"
 out_path = "C:\\Users\\ebrandes\\Documents\\DNDC\\switchgrass_integration.gdb"
-out_name = "subfield_profit_2012_2014_IA151"
+out_name = "subfield_profit_surface_2012_2014"
 arcpy.TableToTable_conversion(in_rows, out_path, out_name)
 
 # clip subfieldIA_single to HUC 12 071000040103 watershed (select in HUC_07100004 layer first)
@@ -52,10 +52,9 @@ arcpy.Clip_analysis(in_features, clip_features, out_feature_class)
 # join profit data to SubfieldHUC071000040103
 in_feature_class = out_feature_class
 in_field = "cluid_mukey" 
-join_table = "subfield_profit_2012_2014_IA151"
+join_table = "subfield_profit_surface_2012_2014"
 join_field = "cluid_mukey"
-field_list = ["profit12", "profit14"]  # is "clumuha" needed?
-
+field_list = ["profit_cg_2012", "profit_sb_2012", "profit_cg_2014", "profit_sb_2014"]
 arcpy.JoinField_management(in_feature_class, in_field, join_table, join_field, field_list)
 
 
