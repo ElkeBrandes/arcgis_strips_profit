dd93c31e416f9a81b78955097398cafd10ebaa94
IASTATE\ebrandes <ebrandes@iastate.edu>
2016-10-21 16:31:21 -0500

    new cmd script to execute script with different arguments

---
+++ StripsProfit02.cmd
@@ -0,0 +1,4 @@
+C:\Python27\ArcGISx6410.3\python.exe C:\Users\ebrandes\Documents\STRIPS_profit\ArcGIS_strips_profit.git\StripsProfit02.py "profit_cg_2012" 
+C:\Python27\ArcGISx6410.3\python.exe C:\Users\ebrandes\Documents\STRIPS_profit\ArcGIS_strips_profit.git\StripsProfit02.py "profit_cg_2014" 
+C:\Python27\ArcGISx6410.3\python.exe C:\Users\ebrandes\Documents\STRIPS_profit\ArcGIS_strips_profit.git\StripsProfit02.py "profit_sb_2012" 
+C:\Python27\ArcGISx6410.3\python.exe C:\Users\ebrandes\Documents\STRIPS_profit\ArcGIS_strips_profit.git\StripsProfit02.py "profit_sb_2014" 
